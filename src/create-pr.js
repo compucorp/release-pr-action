6203e672c5dc080f177aab4d8ba72259853aaf48
Debarshi Bhaumik <debarshi@compucorp.co.uk>
2021-02-17 13:43:01 +0530

    CPS-464: Add readme files

--- src/create-pr.js
+++ src/create-pr.js
@@ -217,7 +217,6 @@ async function getAllMergeCommitsSinceBeginning () {
 async function getPullRequests (lastRelease) {
   let commits;
 
-  var lastRelease = { tag_name: '1.9.9'};
   if (lastRelease) {
     commits = await getMergeCommitsSince(lastRelease.tag_name);
   } else {
